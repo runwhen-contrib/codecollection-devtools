a59bd96fcd019d20445b14a650c3f08a81195df9
jon-funk <jonathan.funk@runwhen.com>
2023-01-06 09:59:45 -0800

    Migrate devtools (#1)

---
+++ dev_facade/RW/Core.py
@@ -0,0 +1,417 @@
+"""
+Core keyword library
+
+Scope: Global
+"""
+
+import re
+import os
+import requests
+from typing import Union, List
+from robot.libraries.BuiltIn import BuiltIn
+from RW import platform
+
+import json
+import textwrap
+from collections import OrderedDict
+
+class Core:
+    """Core keyword library defines keywords used to access key platform features from robot code."""
+
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+
+    def __init__(self) -> None:
+        self.builtin = BuiltIn()  # TODO - use get library instance
+
+    def import_secret(self, varname: str, description: str = None, example: str = None, pattern: str = None, **kwargs):
+        skeys_json_str = os.getenv("RW_SECRET_REMAP", "{}")
+        fkeys_json_str = os.getenv("RW_FROM_FILE", "{}")
+        secret_remaps = json.loads(skeys_json_str)
+        secrets_from_files = json.loads(fkeys_json_str)
+        if varname in secret_remaps:
+            key = secret_remaps.get(varname)
+        else:
+            key = varname
+
+        if key in secrets_from_files.keys():
+            secret_filepath = secrets_from_files[key]
+            with open(secret_filepath) as fh:
+                val = fh.read()
+        else:
+            val = os.getenv(key, "")
+        ret = platform.Secret(varname, val)
+        self.builtin.set_suite_variable("${" + varname + "}", ret)
+        return ret
+
+    def import_service(
+        self, varname: str, description: str = None, example: str = None, default: str = None, **kwargs
+    ):
+        """Creates an instance of rwplatform.Service for use by other keywords.
+
+        Note that the description, example, default args are parsed in RunWhen static
+        analysis for type hinting in the GUI, and are not (currently) used here.
+        """
+        env_var_url = "RW_SVC_URLS"
+        urls_json_str = os.getenv(env_var_url)
+        if not urls_json_str:
+            raise ImportError(
+                f"Import Service {varname}: No services provided in configuration ({env_var_url} has no value set)"
+            )
+        url = json.loads(urls_json_str).get(varname)
+        if not url:
+            raise ImportError(f"Import Service {varname}: No service provided, found only ({urls_json_str})")
+        ret = platform.Service(url)
+        self.builtin.set_suite_variable("${" + varname + "}", ret)
+        return ret
+
+    def import_user_variable(
+        self,
+        varname: str,
+        type: str = "string",
+        description: str = None,
+        example: str = None,
+        pattern: str = None,
+        enum: str = None,
+        format: str = None,
+        default: str = None,
+        **kwargs,
+    ) -> str:
+        """
+        Imports a variable set by the user, raises error if not available.
+        When locally run these correspond to environment variables.
+        The user may remap what environment key names are imported by setting the `RW_ENV_REMAP` environment variable.
+
+        Example:
+          Import User Variable   FOO
+          Debug Log              ${FOO}
+
+        Throws an error if the config variable doesn't exist and no default is
+        provided (Implementation subject to change)
+
+        Impl note - the optional args correspond to JSONSchema / OpenAPIv3 properties that are used in RW's static
+        analysis of robot code to do type hinting and validation in the ui and pre-commit hooks, i.e. they
+        are not (currently) consumed in this code.  For type, we currently support "string", "boolean", "number",
+        "integer".  Description and Example should be short phrase or single sentence strings.  Enum, for Robot
+        ease-of-use, should be a string of comma-separated values (typically strings themselves) without escaped
+        quotes or brackets, e.g. the python call would look like enum="option-1,option-2,option-3" and the Robot
+        call looks like enum=option-1,option-2,option-3.
+        """
+        ekeys_json_str = os.getenv("RW_ENV_REMAP", "{}")
+        env_remaps = json.loads(ekeys_json_str)
+        if varname in env_remaps:
+            key = env_remaps.get(varname)
+            val = os.getenv(key, "")
+        else:
+            val = os.getenv(varname, "")
+        if default and not val:
+            val = default
+        self.builtin.set_suite_variable("${" + varname + "}", val)
+        return val
+
+    def run_keyword_and_push_metric(self, name: str, *args) -> None:
+        """
+        Run a keyword and push the returned metric up to the MetricStore.
+        This should only be called once per sli.robot file.
+
+        Example:
+          Run Keyword and Push Metric   Ping Hosts and Return Highest Avg RTT
+          ...                           hosts=${PING_HOSTS}
+        """
+        self.debug_log(f"Running keyword: {name}, arguments: {args}")
+        result = self.builtin.run_keyword(name, *args)
+        self.debug_log(f"Push metric result: {result}")
+        return self.push_metric(result)
+
+    GAUGE = "gauge"
+    COUNTER = "counter"
+    HISTOGRAM = "histogram"
+    UNTYPED = "untyped"
+
+    def push_metric(
+        self,
+        value=None,
+        sub_name=None,
+        metric_type=UNTYPED,
+        dry_run=False,
+        **kwargs,
+    ):
+        """
+        Used to push a metric up to the MetricStore.  Each SLX has a single default metric
+        by default that all SLIs should use where the sub_name should be set to None.
+        Callers may also use this method multiple times in a single SLI with subsequent
+        metric sub_name args set to various strings in order to push multiple metrics in
+        addition to the default.
+
+        Examples:
+            #Base case test
+            Push Metric   10
+
+            #Try with a sub-name
+            Push Metric   11      sub_name=some_non_default_metric
+
+            #Try various types
+            Push Metric   12      sub_name=as_gauge  metric_type=gauge
+            Push Metric   13      sub_name=as_counter  metric_type=counter
+
+            #Try with labels
+            Push Metric   12      sub_name=with_labels  a_label=a    b_label=b     c_label=c
+
+        Example:
+          Push Metric	5
+          Push Metric	5	http_code=200 #Pushes a metric names
+          Push Metric   10  sub_name=foo    type=${GAUGE}   http_code=300 #Pushes a non-defualt metric
+
+        Note that calls to Push Metric are not exactly atomic. Once a metric/sub-metric is pushed
+        with a set of labels in the scope of a Suite, it is expected that any
+        subsequent calls have the same set of labels.  Since it would be very
+        odd (an error?) to have any metric/sub-metric called in more than one place per Suite,
+        this doesn't seem like a constraint in practice.
+        """
+        #Note that during local dev this simply logs to the console.
+        self.builtin.log_to_console(f"\nPush metric: value:{value} sub_name:{sub_name} metric_type:{metric_type} labels:{kwargs}\n")
+
+    def task_failure(self, msg: str) -> None:
+        """
+        Report a validation failure. Skip to the next task/test.
+
+        :param msg: Log message
+        """
+        raise platform.TaskFailure(msg)
+
+    def task_error(self, msg: str) -> None:
+        """
+        Report an error in execution. Skip to the next task/test.
+
+        :param msg: Log message
+        """
+        raise platform.TaskError(msg)
+
+    def fatal_error(self, msg: str) -> None:
+        """
+        Report a fatal error. Stop the whole robot execution.
+
+        :param msg: Log message
+        """
+        raise platform.FatalError(msg)
+
+    def error_log(self, *args, **kwargs) -> None:
+        """
+        Error log
+
+        :param msg: Log message
+        """
+        platform.error_log(*args, **kwargs)
+
+    def warning_log(self, *args, **kwargs) -> None:
+        """
+        Warning log
+
+        :param msg: Log message
+        """
+        platform.warning_log(*args, **kwargs)
+
+    def info_log(self, *args, **kwargs) -> None:
+        """
+        Info log
+
+        :param msg: Log message
+        """
+        platform.info_log(*args, **kwargs)
+
+    def inspect_object_attributes(self, d, console: Union[str, bool] = False) -> None:
+        platform.debug_log(dir(d), console=console)
+
+    def debug_log(self, *args, **kwargs) -> None:
+        """
+        Debug log
+
+        :param str: Log message
+        :param console: Write log message to console (default is true)
+        """
+        platform.debug_log(*args, **kwargs)
+
+    def trace_log(self, *args, **kwargs) -> None:
+        """
+        Trace log
+
+        :param msg: Log message
+        """
+        platform.trace_log(*args, **kwargs)
+
+    def console_log(self, *args, **kwargs) -> None:
+        platform.console_log(*args, **kwargs)
+
+    def console_log_if_true(self, *args, **kwargs) -> None:
+        """
+        If the condition is evaluated to true, the message is written to the
+        console.
+
+        :param condition: Condition to evaluate as a Python expression
+        :param msg: Log message
+        """
+        platform.console_log_if_true(*args, **kwargs)
+
+    def add_to_report(
+        self,
+        obj: object,
+        fmt: str = "p",
+        **kwargs,
+    ) -> None:
+        """
+        Generic keyword used to add to reports.  The common case is adding a string
+        with "p" formatting, but this is intended to be extensible to include pre-formatted
+        blocks, code blocks, links and potentially chart data.
+        """
+        # for local dev, simply output to the console
+        self.builtin.log_to_console(f"\n{obj}\n")
+
+    def add_code_to_report(self, obj: str) -> None:
+        """Add a block of text to the report that should follow
+        similar formatting rules as the html tag "code"
+        """
+        return self.add_to_report(obj=str(obj), fmt="code")
+
+    def add_pre_to_report(self, obj: str) -> None:
+        """Add a block fo text to the report that should follow
+        similar formatting rules as the html tag "pre"
+        """
+        return self.add_to_report(obj=str(obj), fmt="pre")
+
+    def add_url_to_report(self, url: str, text: str = None) -> None:
+        """Add a url fo text to the report that should follow
+        similar formatting rules as the html tag "pre"
+        """
+        return self.add_to_report(obj=str(url), fmt="a", text=text)
+
+    def add_json_to_report(self, obj) -> None:
+        """Add a json string or json serializable object to the report implying to
+        most formatters that it shoudl be pretty printed.  Internally this is stored
+        as in object rather than string form.
+        """
+        if isinstance(obj, str):  # If we got a string, make sure it is valid json
+            obj = json.loads(obj)
+        elif isinstance(obj, object):  # If we got an object, make sure it serializes safely
+            json.dumps(obj)
+        return self.add_to_report(obj=obj, fmt="json")
+
+    def add_table_to_report(self, about: str, body: [], head: []) -> None:
+        """Adds a table of data to the report.  The 'about' string should be
+        rendered to the left, right or below the table.  The body is expected to
+        be a 2d array of strings.  head is a 1d array of strings.  The gist
+        is to map closesly to an html table element.
+        """
+        body_o = body
+        head_o = head
+        return self.add_to_report(obj=about, fmt="table", body=body_o, head=head_o)
+
+    def add_datagrid_to_report(
+        self,
+        about: str,
+        rows: [],
+        columns: [],
+        page_size: int,
+        rows_per_page_options: [int],
+    ) -> None:
+        """Adds an object that will map closely to a MUI datagrid.  For args,
+        see https://mui.com/x/react-data-grid/
+        """
+        return self.add_to_report(
+            obj=str(about),
+            fmt="table",
+            rows=rows,
+            columns=columns,
+            page_size=page_size,
+            rows_per_page_options=rows_per_page_options,
+        )
+
+    def get_report_data(self) -> object:
+        """Return the data for this report as an object (not formatted)"""
+        return self._report
+
+    def get_report_data_as_string(self) -> str:
+        """Return the data for this report (with all formatting hints)
+        as a string
+        """
+        return json.dumps(self._report, indent=2)
+
+    def _code_to_string(self, obj) -> str:
+        """Converts a "code" obj to a string"""
+        return str(obj)
+
+    def _pre_to_string(self, obj) -> str:
+        """Converts a "pre" obj to a string"""
+        return str(obj)
+
+    def _p_to_string(self, obj) -> str:
+        """Converts a "p" obj to a string"""
+        lines = textwrap.wrap(" ".join(str(obj).split()))
+        return "\n".join(lines)
+
+    def _a_to_string(self, obj, text) -> str:
+        """Converts a "a" obj to a string"""
+        if text:
+            return f"{text} ({obj})"
+        else:
+            return f"{obj}"
+
+    def _json_to_string(self, obj) -> str:
+        """Converts a "json" string or json-serializable object to a (prettified) string"""
+        if isinstance(obj, str):
+            obj = json.loads(obj)
+        return json.dumps(obj, indent=2)
+
+    def _table_to_string(self, about, body, head) -> str:
+        """Converts a "table" obj to a string"""
+        max_lens = []  # Calculate max length of the text in each column
+        for col in range(0, len(head)):
+            max_lens.append(len(str(head[col])))
+        for row in body:
+            for col in range(0, len(row)):
+                max_lens[col] = max(max_lens[col], len(str(row[col])))
+        ret = []
+        ret_row = []
+        for i in range(0, len(head)):
+            txt = head[i]
+            max_len = max_lens[i]
+            ret_row.append(f"{txt:>{max_len}}")
+        ret.append("|".join(ret_row))
+        for row in body:
+            ret_row = []
+            for i in range(0, len(row)):
+                txt = row[i]
+                max_len = max_lens[i]
+                ret_row.append(f"{txt:>{max_len}}")
+            ret.append("|".join(ret_row))
+        ret.append(about)
+        return "\n".join(ret)
+
+    def _datagrid_to_string(self, about, rows, columns) -> str:
+        return self._json_to_string(rows)
+
+    def shell(
+        self,
+        cmd: str,
+        service: platform.Service,
+        request_secrets: List[platform.ShellServiceRequestSecret] = None,
+        secret: platform.Secret = None,
+        secret_as_file: bool = False,
+        env: dict = None,
+        files: dict = None,
+    ):
+        """Robot Keyword to expose rwplatform.execute_shell_command.
+
+        For robot syntax convenience, a single secret and secret_as_file may be given instead of or in addition
+        to the request_secrets arg (an array of ShellServiceRequestSecrets).  This will wrap secret and secret_as_file in
+        to a one-item list of ShellServiceRequestSecrets, adding the request_secrets list if one was given.
+        """
+        if not isinstance(service, platform.Service):
+            raise ValueError(
+                f"service {service} is not an instance of rwplatform.Service - check arg type from Robot, see import_service"
+            )
+        request_secrets_p = request_secrets[:] if request_secrets else []
+        if secret:
+            ssrs = platform.ShellServiceRequestSecret(secret=secret, as_file=secret_as_file)
+            request_secrets_p.append(ssrs)
+        return platform.execute_shell_command(
+            cmd=cmd, service=service, request_secrets=request_secrets_p, env=env, files=files
+        )
---
+++ dev_facade/RW/platform.py
@@ -0,0 +1,253 @@
+"""A set of python interfaces also exposed in RW.Core (and used outside of that)"""
+import requests, os, pprint, traceback, json
+from typing import Union, Optional, List
+from dataclasses import dataclass, field
+from robot.api import logger as user_logger
+from robot.api import Failure, Error, FatalError, logger
+from robot.libraries.BuiltIn import BuiltIn
+
+import logging
+
+platform_logger = logging.getLogger(__name__)
+robot_builtin = BuiltIn()
+
+session = None
+
+
+class TemporaryException(Exception):
+    pass
+
+
+class PermanentException(Exception):
+    pass
+
+
+class InputException(PermanentException):
+    pass
+
+
+class Secret:
+    """The secret class is used as a wrapper around secret values to track their usage
+    and to make sure they don't accidentally escape in to logs as strings.  Keyword
+    authors should take instances of Secret as arguments when they suspect that the
+    content of the string is sensitive, prompting Robot authors to add import secret
+    commands and flag this sensitivity to users.
+    """
+
+    def __init__(self, key: str, val: str):
+        self._key = key
+        self._val = val
+
+    @property
+    def value(self):
+        stack = traceback.format_stack()
+        robot_builtin.log(f"secret {self._key} accessed from callstack {stack}")
+        return self._val
+
+    @property
+    def key(self):
+        return self._key
+
+    def __str__(self):
+        return "*" * len(self.value)
+
+
+@dataclass(frozen=True)
+class Service:
+    """The secret class is used as a wrapper around service URLs, created by
+    the Import Service keyword.  (Over time, the gist is to offer health
+    check / status / version params as well as providing the basic URL
+    for robot authors.)
+    """
+
+    url: str
+
+    def health_check(self):
+        """A stub implementation for now, should raise an Exception
+        if the service is not currently healthy.
+        """
+        return True
+
+
+@dataclass(frozen=True)
+class ShellServiceRequestSecret:
+    secret: Secret
+    as_file: bool = False
+
+
+@dataclass(frozen=True)
+class ShellServiceRequest:
+    cmd: str
+    request_secrets: List[ShellServiceRequestSecret] = field(default_factory=lambda: [])
+    env: dict = field(default_factory=lambda: {})
+    files: dict = field(default_factory=lambda: {})
+    timeout_seconds: int = 60
+
+    def to_json(self):
+        """Serialize this request out to json appropriate for shell service post body.  (The default
+        todict implementation of dataclasses doesn't serialize the request_secrets, so this
+        replaces it for the common use case.)
+        """
+        # Without the nested dataclass or camel case, this would be json.dumps(self.todict()),
+        # but we have both
+        obj = {}
+        obj["cmd"] = self.cmd
+        if self.request_secrets:
+            obj["secrets"] = [
+                {"key": s.secret.key, "value": s.secret.value, "file": s.as_file} for s in self.request_secrets
+            ]
+        if self.files:
+            obj["files"] = self.files
+        if self.env:
+            obj["env"] = self.env
+        return json.dumps(obj)
+
+
+@dataclass(frozen=True)
+class ShellServiceResponse:
+    cmd: str  # The original cmd string given
+    parsed_cmd: str = None  # Useful for debugging long commands
+    stdout: str = None  # stdout from running cmd
+    stderr: str = None  # stderr from running cmd
+    returncode: int = -1  # The returncode from running cmd
+    status: int = 500  # The http status code from the service, representing any errors
+    # the plumbing pre/post command
+    body: str = ""  # The raw body of the response as a string for troubleshooting plumbing errors
+    # A list of strings with error messages from the plumbing and cmd results
+    errors: List[str] = field(default_factory=lambda: [])
+
+    @staticmethod
+    def from_json(obj, status_code=200):
+        """De-serialize this request from a json obj found in shell service http response and the status_code"""
+        if isinstance(obj, list):
+            if len(obj) != 1:
+                raise ValueError(
+                    f"Trying to parse JSON string in to ShellServiceResponse, but"
+                    + f"JSON string had more than one object {str(obj)}"
+                )
+            else:
+                obj = obj[0]
+        # Note conversion from camelcase is required (and not worth another dependency IMHO)
+        try:
+            ret = ShellServiceResponse(
+                cmd=obj["cmd"],
+                parsed_cmd=obj["parsedCmd"],
+                stdout=obj["stdout"],
+                stderr=obj["stderr"],
+                returncode=obj["returncode"],
+                status=status_code,
+            )
+            return ret
+        except KeyError as ke:
+            raise TaskError(
+                f"Error parsing shell service response {type(ke)}: {ke} from object {obj} and status code {status_code}"
+            )
+
+
+class TaskFailure(Failure):
+    """
+    This exception can be raised for a task failure due to a failed
+    validation.
+    """
+
+
+class TaskError(Error):
+    """
+    This exception can be raised for a task error caused by a malfunction
+    or unexpected result.
+    """
+
+def error_log(*msg, console: Union[bool, str] = False, if_true: Optional[str] = None) -> None:
+    """
+    Note: Error logs are automatically written to console.
+    """
+    if if_true is not None and BuiltIn().evaluate(if_true) is not True:
+        return
+    _ = console
+    for s in msg:
+        if not isinstance(s, str):
+            s = pprint.pformat(s, indent=1, width=80)
+        if console or isinstance(console, str) and console.lower() == "true":
+            robot_builtin.log_to_console(f"\n{str(s)}")
+        logger.error(str(s))
+
+
+def warning_log(*msg, console: Union[bool, str] = False, if_true: Optional[str] = None) -> None:
+    """
+    Note: Warning logs are automatically written to console.
+    """
+    if if_true is not None and BuiltIn().evaluate(if_true) is not True:
+        return
+    _ = console
+    for s in msg:
+        if not isinstance(s, str):
+            s = pprint.pformat(s, indent=1, width=80)
+        if console or isinstance(console, str) and console.lower() == "true":
+            robot_builtin.log_to_console(f"\n{str(s)}")
+        logger.warn(str(s))
+
+
+def info_log(*msg, console: Union[bool, str] = False, if_true: Optional[str] = None) -> None:
+    if if_true is not None and BuiltIn().evaluate(if_true) is not True:
+        return
+    _ = console
+    for s in msg:
+        if not isinstance(s, str):
+            s = pprint.pformat(s, indent=1, width=80)
+        if console or isinstance(console, str) and console.lower() == "true":
+            robot_builtin.log_to_console(f"\n{str(s)}")
+        logger.info(str(s))
+
+
+def debug_log(*msg, console: Union[bool, str] = False, if_true: Optional[str] = None) -> None:
+    if if_true is not None and BuiltIn().evaluate(if_true) is not True:
+        return
+    _ = console
+    for s in msg:
+        if not isinstance(s, str):
+            s = pprint.pformat(s, indent=1, width=80)
+        if console or isinstance(console, str) and console.lower() == "true":
+            robot_builtin.log_to_console(f"\n{str(s)}")
+        logger.debug(str(s))
+
+
+def trace_log(*msg, console: Union[bool, str] = False, if_true: Optional[str] = None) -> None:
+    if if_true is not None and BuiltIn().evaluate(if_true) is not True:
+        return
+    _ = console
+    for s in msg:
+        if not isinstance(s, str):
+            s = pprint.pformat(s, indent=1, width=80)
+        if console or isinstance(console, str) and console.lower() == "true":
+            robot_builtin.log_to_console(f"\n{str(s)}")
+        logger.trace(str(s))
+
+
+def console_log(*msg) -> None:
+    info_log(*msg, console=True)
+
+
+def console_log_if_true(condition: str, *msg) -> None:
+    info_log(msg, console=True, if_true=condition)
+
+
+def execute_shell_command(
+    cmd: str,
+    service: Service,
+    request_secrets: List[ShellServiceRequestSecret] = None,
+    env: dict = None,
+    files: dict = None,
+):
+    ss_req = ShellServiceRequest(cmd=cmd, request_secrets=request_secrets, env=env, files=files)
+    url = service.url + "/api/v1/cmd"
+    headers = {"Content-type": "application/json", "Accept": "application/json"}
+    try:
+        rsp = requests.post(url, data=ss_req.to_json(), headers=headers)
+        response_obj = rsp.json()
+        ss_rsp = ShellServiceResponse.from_json(response_obj, rsp.status_code)
+        logger.debug(f"execute_shell_command with shell service requrest: {ss_req} and received response {ss_rsp}")
+        return ss_rsp
+    except requests.JSONDecodeError as e:
+        raise TaskFailure(
+            f"JSON Decode Error {type(e)}: {e} trying to parse shell service response from response {rsp} with body {rsp.text}"
+        ) from e
