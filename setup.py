d6e75c49ade37d4fcf1841feaedfa9a50ede460c
stewartshea <>
2024-11-06 16:19:44 -0500

    packaging updates

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-from setuptools import setup, find_packages
-
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
+from setuptools import setup
 
 setup(
     name="rw-devtools",
     version=open("VERSION").read().strip(),
-    packages=find_packages(where="dev_facade", include=["RW*"]),
-    package_dir={"": "dev_facade"},
+    packages=["RW"],  # Explicitly list the `RW` package
+    package_dir={"RW": "dev_facade/RW"},  # Map `RW` to its directory
     license="Apache License 2.0",
     description="A set of RunWhen Developer keywords and python libraries for local development",
     long_description=open("README.md").read(),
@@ -15,11 +12,10 @@ setup(
     author="RunWhen",
     author_email="info@runwhen.com",
     url="https://github.com/runwhen-contrib/codecollection-devtools",
-    install_requires=required,
+    install_requires=open("requirements.txt").read().splitlines(),
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
     ],
 )
-
